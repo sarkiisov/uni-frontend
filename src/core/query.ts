db4fc8b67f2b3929231924f8aa08bdf6111036a1
sarkisovTu <sarkisov1337@gmail.com>
2023-10-02 22:45:48 +0300

    feat: return data from axios response

--- src/core/query.ts
+++ src/core/query.ts
@@ -2,21 +2,23 @@ import { QueryClient, QueryFunctionContext } from '@tanstack/react-query'
 import { AxiosRequestConfig } from 'axios'
 import { axiosInstance } from './axios'
 
-const defaultQueryFn = ({ queryKey }: QueryFunctionContext) => {
-  const [basePath, axiosRequestConfig = {}] = queryKey as [
-    string,
-    AxiosRequestConfig & { path?: string }
-  ]
+export type DefaultQueryKey = [
+  string,
+  (AxiosRequestConfig & { path?: string })?
+]
+
+const defaultQueryFn = async ({ queryKey }: QueryFunctionContext) => {
+  const [basePath, axiosRequestConfig = {}] = queryKey as DefaultQueryKey
 
   const { path, ...axiosParams } = axiosRequestConfig
 
   try {
-    const data = axiosInstance({
+    const data = await axiosInstance({
       url: `${basePath}${path ? (path.startsWith('/') ? path : `/${path}`) : ''}`,
       method: 'get',
       ...axiosParams
     })
-    return Promise.resolve(data)
+    return await Promise.resolve(data.data)
   } catch (error) {
     return Promise.reject(error)
   }
