f5052a767210b00b5476ace4580dcd9a2e565790
sarkisovTu <sarkisov1337@gmail.com>
2023-10-16 22:02:12 +0300

    feat: prozy backend requests

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   },
