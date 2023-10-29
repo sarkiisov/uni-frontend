fd3072bba5f48b13fe34b2b4352ec6187039770c
sarkisovTu <sarkisov1337@gmail.com>
2023-10-29 12:20:08 +0300

    chore(app): loader catch network error

--- src/modules/app/loaders/appLoader.ts
+++ src/modules/app/loaders/appLoader.ts
@@ -1,24 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { redirect } from 'react-router-dom'
 import { makeLoader } from 'react-router-typesafe'
 import { queryClient } from '@/core'
 import { userQuery, userStatusQuery } from '@/modules/auth/queries'
-import { UserStatus } from '@/modules/auth/types'
+import { User, UserStatus } from '@/modules/auth/types'
 
 export const appLoader = makeLoader(async () => {
   try {
-    const userStatus = (
-      queryClient.getQueryData(userStatusQuery().queryKey) ??
-      await queryClient.fetchQuery(userStatusQuery())) as UserStatus
+    try {
+      const userStatus = (
+        queryClient.getQueryData(userStatusQuery().queryKey) ??
+        await queryClient.fetchQuery(userStatusQuery())) as UserStatus
 
-    if (!userStatus.hasTest) {
-      return redirect('/quiz')
-    }
-    if (!userStatus.hasInfo) {
-      return redirect('/info')
-    }
+      if (!userStatus.hasTest) {
+        return redirect('/quiz')
+      }
+      if (!userStatus.hasInfo) {
+        return redirect('/info')
+      }
 
-    return queryClient.getQueryData(userQuery().queryKey) ??
-    await queryClient.fetchQuery(userQuery())
+      const user = queryClient.getQueryData(userQuery().queryKey) ??
+      await queryClient.fetchQuery(userQuery()) as User
+
+      return null
+    } catch (error) {
+      return Promise.reject(error)
+    }
   } catch (error) {
     return redirect('/login')
   }
