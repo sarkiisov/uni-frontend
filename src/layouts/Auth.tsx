8794be1b3b782646c9113a1abe9e5591fe51a5a9
sarkisovTu <sarkisov1337@gmail.com>
2023-10-30 22:54:55 +0300

    feat: App & Auth layouts

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,10 +1,14 @@
-import { Card, Center } from '@mantine/core'
+import { Card, Center, Stack } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
+import { Logo } from '@/components'
 
 export const AuthLayout = () => (
   <Center h="100vh">
-    <Card withBorder w="26rem" padding={0} radius="sm" shadow="xs">
-      <Outlet />
-    </Card>
+    <Stack gap="xl">
+      <Logo />
+      <Card withBorder w="26rem" padding={0} radius="sm" shadow="xs">
+        <Outlet />
+      </Card>
+    </Stack>
   </Center>
 )
