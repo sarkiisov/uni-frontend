355f4344eb362599bf8273345ae9cf06959307ef
sarkisovTu <sarkisov1337@gmail.com>
2023-11-15 19:51:51 +0300

    feat: disable PasswordInput eye in Edge browser

--- src/libs/react-hook-form/PasswordInput/PasswordInput.tsx
+++ src/libs/react-hook-form/PasswordInput/PasswordInput.tsx
@@ -1,8 +1,8 @@
 import { useController, useFormContext } from 'react-hook-form'
 import { PasswordInput as MantinePasswordInput } from '@mantine/core'
-import { Eye, EyeOff } from 'lucide-react'
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import { PasswordInputProps } from './types'
+import classes from './PasswordInput.module.css'
 
 export const PasswordInput = ({
   name, shouldUnregister, readOnly, ...props
@@ -16,17 +16,13 @@ export const PasswordInput = ({
 
   const [visibility, setVisibility] = useState(false)
 
-  const VisibilityIcon = useMemo(() => ({ reveal }: { reveal: boolean }) => {
-    const Icon = reveal ? Eye : EyeOff
-
-    return <Icon size={18} strokeWidth={1.35} />
-  }, [visibility])
-
   return (
     <MantinePasswordInput
+      classNames={{
+        input: classes.input
+      }}
       visible={visibility}
       onVisibilityChange={setVisibility}
-      visibilityToggleIcon={VisibilityIcon}
       value={value ?? ''}
       error={error?.message}
       readOnly={formState.isSubmitting || readOnly}
