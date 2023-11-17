2ed0e4679dd42aab39564fa5fd858c172a66670d
sarkisovTu <sarkisov1337@gmail.com>
2023-11-17 21:37:46 +0300

    feat: dayjs extend CustomParseFormat

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import dayjs from 'dayjs'
+import CustomParseFormat from 'dayjs/plugin/customParseFormat'
 import { AppRouter } from './AppRouter'
 
 import 'dayjs/locale/ru'
@@ -10,6 +12,8 @@ import '@mantine/dates/styles.css'
 import '@mantine/carousel/styles.css'
 
 import { RootProvider } from './providers'
+
+dayjs.extend(CustomParseFormat)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
