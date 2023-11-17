28cdfd253350dbd374c816e36142d53da14136bb
sarkisovTu <sarkisov1337@gmail.com>
2023-11-17 22:03:59 +0300

    fix(auth): logout removeQueries

--- src/modules/auth/utils/logout.ts
+++ src/modules/auth/utils/logout.ts
@@ -1,15 +1,6 @@
-import { queryClient } from '@/core'
 import { setToken, setUser } from '../store'
-import { userQuery, userStatusQuery } from '../queries'
 
 export const logout = () => {
   setUser(undefined)
   setToken(undefined)
-
-  queryClient.removeQueries({
-    queryKey: userQuery().queryKey
-  })
-  queryClient.removeQueries({
-    queryKey: userStatusQuery().queryKey
-  })
 }
